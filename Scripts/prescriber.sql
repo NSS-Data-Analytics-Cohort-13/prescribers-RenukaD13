8924cfb8a02081e12e6c60b6fad9b1db522a22ff
RenukaD13 <renuka.rs.deshmukh@gmail.com>
2024-10-11 22:55:38 -0500

    SQL presciber final push Oct11 10:55pm

--- Scripts/prescriber.sql
+++ Scripts/prescriber.sql
@@ -128,29 +128,107 @@ GROUP BY drug_type
 
 --Q5 a. How many CBSAs are in Tennessee? **Warning:** The cbsa table contains information for all states, not just Tennessee.
 
---SELECT COUNT(*) cbsa_tn_count
-SELECT * 
-FROM cbsa 
-WHERE cbsaname LIKE ('%TN%')
-/*Answer = There are 56 CBSA's present in Tennessee*/
+SELECT COUNT(*) 
+FROM fips_county AS f
+INNER JOIN cbsa AS c
+ON f.fipscounty = c.fipscounty
+WHERE f.state = 'TN'
+--Answer = There are 42 CBSA's present in Tennessee
 
 -- Q5 b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
 
 SELECT 
-	c.cbsaname, 
-	SUM(p.population) as total_population
-FROM cbsa AS c
-LEFT JOIN zip_fips as z
-	ON c.fipscounty = z.fipscounty
-JOIN population AS p
-	ON  z.fipscounty =  p.fipscounty
-WHERE c.cbsaname LIKE ('%TN%')
-GROUP BY c.cbsaname
-ORDER BY total_population DESC
---Answer largest combined population = "Memphis, TN-MS-AR" and Total Population is "67870189"
---  largest combined population = "Morristown, TN" and Total Population is "1163520"
+	cb.cbsaname, 
+	SUM(p.population) as combined_population
+FROM fips_county AS f
+INNER JOIN cbsa AS cb 
+	ON f.fipscounty = cb.fipscounty
+INNER JOIN population AS p 
+	ON f.fipscounty = p.fipscounty
+WHERE f.state = 'TN' 
+GROUP BY cb.cbsaname
+ORDER BY combined_population
+/* Answer largest combined population = "Nashville-Davidson_Murfreesboro-Franklin" and Total Population is "1830410"
+  smallest combined population = "Morristown, TN" and Total Population is "116352" */
+ 
+-- Q5 c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
 
--- Q5 c.What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
+SELECT 
+	f.county, 
+	SUM(p.population) as combined_population
+FROM fips_county AS f
+INNER JOIN population AS p 
+	ON f.fipscounty = p.fipscounty
+WHERE f.fipscounty IN
+		--Subquery to return TN fipscounty which are not included in CBSA
+		(SELECT fipscounty FROM fips_county WHERE STATE = 'TN' --fips_county table has 96 records for TN
+		EXCEPT
+		SELECT fipscounty FROM cbsa) --Total 54 fipscounty are not present in CBSA
+GROUP BY f.county
+ORDER BY combined_population desc
+LIMIT 1
+/* Answer = "SEVIER" county with 95523 population is the largest county in terms of population, which is not included in a CBSA*/
+
+--Q6 a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
+
+SELECT drug_name, total_claim_count 
+FROM prescription 
+WHERE total_claim_count >=3000
+/* Answer : 9 rows*/
+
+--Q6 b. For each instance that you found in part a, add a column that indicates whether the drug is an opioid.
+
+SELECT 
+	p1.drug_name, 
+	p1.total_claim_count, 
+	d.opioid_drug_flag
+FROM prescription AS p1
+INNER JOIN drug AS d
+	ON d.drug_name = p.drug_name 
+WHERE p.total_claim_count >=3000
+
+--Q6 c. Add another column to you answer from the previous part which gives the prescriber first and last name associated with each row.
+
+SELECT 
+	p1.drug_name, 
+	p1.total_claim_count, 
+	d.opioid_drug_flag,
+	p2.nppes_provider_first_name, 
+	p2.nppes_provider_last_org_name
+FROM prescription AS p1
+INNER JOIN drug AS d
+	ON d.drug_name = p1.drug_name 
+INNER JOIN prescriber AS p2
+	USING(npi)
+WHERE p1.total_claim_count >=3000
+
+--7. The goal of this exercise is to generate a full list of all pain management specialists in Nashville and the number of claims they had for each opioid. **Hint:** The results from all 3 parts will have 637 rows.
+
+--Q7 a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Management) in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). **Warning:** Double-check your query before running it. You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
+
+
+ SELECT p1.npi, d.drug_name
+, p1.nppes_provider_first_name, p1.nppes_provider_last_org_name
+ FROM prescriber AS p1
+ INNER JOIN prescription AS p2
+ 	USING(npi)
+	INNER JOIN drug AS d
+	ON p2.drug_name = d.drug_name 
+ WHERE p1.specialty_description = 'Pain Management'
+ AND p1.nppes_provider_city = 'NASHVILLE'
+ order by 1,3,4
+/*Answer = 89 rows*/
+
+--Q7 b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. You should report the npi, the drug name, and the number of claims (total_claim_count).
+
+SELECT p1.npi, p2.drug_name, p2.total_claim_count
+FROM prescriber AS p1
+LEFT JOIN prescription AS p2 USING(npi)
+WHERE p1.specialty_description = 'Pain Management'
+AND p1.nppes_provider_city = 'NASHVILLE'
+
+--Q7 c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. Hint - Google the COALESCE function.
+--Answer No NULL values found to convert it into 0
 
 
 
